95c04e2c0cd03ce78230b0ce02040b7879df10b1
Nurmuhammad <obidjonovnurmuhammad4@gmail.com>
2021-04-27 17:55:44 +0500

    Starter app

---
+++ app/src/main/java/com/example/starter/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.starter
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.random.Random as Random1
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
